19ee9daee459ed98bdf18aeb8fec7a79549172fd
sezgink <sezgink@users.noreply.github.com>
2018-01-26 23:12:48 +0300

    Multiplayer Player Control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour{
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+	// Use this for initialization
+	void Start () {
+		if (!isLocalPlayer) {
+			return;
+		} else {
+			GetComponent<AudioListener> ().enabled = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, 0, -x);
+		transform.Translate(0, z, 0);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			CmdFire();
+		}
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			PlayS();
+		}
+	}
+	void PlayS() {
+	
+	}
+	[Command]
+	void CmdFire()
+	{
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate(
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		//bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.up * 50;
+
+		NetworkServer.Spawn (bullet);
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f);        
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<SpriteRenderer>().color = Color.blue;
+	}
+}
