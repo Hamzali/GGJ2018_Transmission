8fa59447a6c775f89b20adcd6a9b7a82d635d886
Hamza Ali <hamzali.tas.95@gmail.com>
2018-01-27 03:20:33 +0300

    Map And Other Managers

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+	// Use this for initialization
+	
+	public GameObject leftWall, rightWall, upWall, downWall;
+
+	public void setWalls(int left, int right, int up, int down) {
+		leftWall.SetActive(right == 0);
+		rightWall.SetActive(left == 0);
+		upWall.SetActive(up == 0);
+		downWall.SetActive(down == 0);
+	}
+}
