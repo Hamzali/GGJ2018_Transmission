e27d0a2571c05b07f1f4dd8936fbeb3574ede9be
Hamza Ali <hamzali.tas.95@gmail.com>
2018-01-27 22:42:04 +0300

    main scene integration

--- Assets/Scripts/BringCamera.cs
+++ Assets/Scripts/BringCamera.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class BringCamera : NetworkBehaviour {
-	Vector3 offset;
+	public Vector3 offset = new Vector3 (0,1.0f,-5);
 	// Use this for initialization
 	void Start () {
 		//offset = Camera.main.gameObject.transform.position - transform.position;
-		offset = new Vector3 (0,1.0f,-10);
+
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class PlayerController : NetworkBehaviour{
+public class PlayerController : NetworkBehaviour {
 	public GameObject bulletPrefab;
 	public Transform bulletSpawn;
 	Rigidbody2D rb;
@@ -21,30 +21,29 @@ public class PlayerController : NetworkBehaviour{
 		if (!isLocalPlayer) {
 			return;
 		} else {
-			
+
 			//Vector2 collisionNormal = new Vector2(Get0,0);
 			GetComponent<AudioListener> ().enabled = true;
 			rb = this.GetComponent<Rigidbody2D> ();
 			ani = GetComponent<Animator> ();
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (!isLocalPlayer)
-		{
+		if (!isLocalPlayer) {
 			return;
 		}
 
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
 
 		//transform.Rotate(0, 0, -x);.
-		transform.position = transform.position + Vector3.right*x*6.0f*Time.deltaTime;
+		transform.position = transform.position + Vector3.right * x * speedMultipler * Time.deltaTime;
 
 		if (Mathf.Abs (x) > 0) {
 			//ani.SetBool ("isMoving", true);	
-			CmdSetAnimator ("isMoving",true);
+			CmdSetAnimator ("isMoving", true);
 			/*if (rb.velocity.magnitude < speedLimit ) {
 				print (!wallCollision);
 				if (wallCollision) {
@@ -59,31 +58,28 @@ public class PlayerController : NetworkBehaviour{
 				CmdFlip (false);
 		} else {
 			//ani.SetBool ("isMoving", false);
-			CmdSetAnimator ("isMoving",false);
-		} 
-
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			CmdFire();
+			CmdSetAnimator ("isMoving", false);
 		}
-		if (Input.GetKeyDown(KeyCode.UpArrow))
-		{
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			CmdFire ();
+		}
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			if (wallCollision) {
 				Jump ();
 			} else {
-				
+
 			}
 		}
 	}
-	void Jump() {
-		rb.velocity += Vector2.up * jumpPowerUp - collisionNormal*jumpPowerNormal;
+	void Jump () {
+		rb.velocity += Vector2.up * jumpPowerUp - collisionNormal * jumpPowerNormal;
 	}
 
 	[Command]
-	void CmdFire()
-	{
+	void CmdFire () {
 		// Create the Bullet from the Bullet Prefab
-		var bullet = (GameObject)Instantiate(
+		var bullet = (GameObject) Instantiate (
 			bulletPrefab,
 			bulletSpawn.position,
 			bulletSpawn.rotation);
@@ -94,72 +90,71 @@ public class PlayerController : NetworkBehaviour{
 		NetworkServer.Spawn (bullet);
 
 		// Destroy the bullet after 2 seconds
-		Destroy(bullet, 2.0f);        
+		Destroy (bullet, 2.0f);
 	}
-	void OnCollisionEnter2D(Collision2D col) {
+	void OnCollisionEnter2D (Collision2D col) {
 		if (isLocalPlayer) {
 			if (col.gameObject.tag == "Wall") {
 				wallCollision = true;
 				//ani.SetBool ("onAir", false);
-				CmdSetAnimator("onAir",false);
+				CmdSetAnimator ("onAir", false);
 				//GetComponent<Animator> ().SetBool ("onAir", false);
-				collisionNormal = (col.contacts [0].point - new Vector2 (transform.position.x, transform.position.y)).normalized;
+				collisionNormal = (col.contacts[0].point - new Vector2 (transform.position.x, transform.position.y)).normalized;
 				print (collisionNormal);
 
 			}
 		}
 	}
-	void OnCollisionStay2D(Collision2D col) {
+	void OnCollisionStay2D (Collision2D col) {
 		if (isLocalPlayer) {
 			if (col.gameObject.tag == "Wall") {
 				wallCollision = true;
 				//ani.SetBool ("onAir", false);	
 				//GetComponent<Animator> ().SetBool ("onAir", false);
-				CmdSetAnimator("onAir",false);
-				collisionNormal = (col.contacts [0].point - new Vector2 (transform.position.x, transform.position.y)).normalized;
+				CmdSetAnimator ("onAir", false);
+				collisionNormal = (col.contacts[0].point - new Vector2 (transform.position.x, transform.position.y)).normalized;
 			}
 		}
 	}
 
-	void OnCollisionExit2D(Collision2D col) {
+	void OnCollisionExit2D (Collision2D col) {
 		if (isLocalPlayer) {
 			if (col.gameObject.tag == "Wall") {
 				//ani.SetBool ("onAir", true);	
-				CmdSetAnimator("onAir",true);
+				CmdSetAnimator ("onAir", true);
 				//GetComponent<Animator> ().SetBool ("onAir", true);
 				wallCollision = false;
 			}
 		}
 	}
+
 	[ClientRpc]
-	void RpcSetAnimator(string key,bool state) {
-		GetComponent<Animator> ().SetBool(key,state);
+	void RpcSetAnimator (string key, bool state) {
+		GetComponent<Animator> ().SetBool (key, state);
 		//ani.SetBool(key,state);
 
 	}
 
 	[Command]
-	void CmdSetAnimator(string key, bool state) {
+	void CmdSetAnimator (string key, bool state) {
 		GetComponent<NetworkIdentity> ().AssignClientAuthority (connectionToClient);
-		RpcSetAnimator (key,state);
+		RpcSetAnimator (key, state);
 		//GetComponent<NetworkIdentity> ().RemoveClientAuthority (connectionToClient);
-
 
 	}
 
 	[ClientRpc]
-	void RpcFlip(bool state) {
+	void RpcFlip (bool state) {
 		GetComponent<SpriteRenderer> ().flipX = state;
 		//ani.SetBool(key,state);
 
 	}
 
 	[Command]
-	void CmdFlip(bool state) {
+	void CmdFlip (bool state) {
 		GetComponent<NetworkIdentity> ().AssignClientAuthority (connectionToClient);
-		RpcFlip(state);
+		RpcFlip (state);
 		//GetComponent<NetworkIdentity> ().RemoveClientAuthority (connectionToClient);
-
 
 	}
 	/*
