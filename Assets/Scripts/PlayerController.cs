491a3ecbe3f784cb954d1a6d9bff010c8b4bc2a9
sezgink <sezgink@users.noreply.github.com>
2018-01-27 03:20:14 +0300

    Added cam follow and sample animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,24 @@ using UnityEngine.Networking;
 public class PlayerController : NetworkBehaviour{
 	public GameObject bulletPrefab;
 	public Transform bulletSpawn;
+	Rigidbody2D rb;
+	bool onAir;
+	bool wallCollision;
+	Vector2 collisionNormal;
+	public float jumpPowerUp;
+	public float jumpPowerNormal;
+	public float speedMultipler;
+	Animator ani;
+	public float speedLimit;
 	// Use this for initialization
 	void Start () {
 		if (!isLocalPlayer) {
 			return;
 		} else {
+			//Vector2 collisionNormal = new Vector2(Get0,0);
 			GetComponent<AudioListener> ().enabled = true;
+			rb = this.GetComponent<Rigidbody2D> ();
+			ani = GetComponent<Animator> ();
 		}
 	}
 	
@@ -25,21 +37,43 @@ public class PlayerController : NetworkBehaviour{
 		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
 		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-		transform.Rotate(0, 0, -x);
-		transform.Translate(0, z, 0);
+		//transform.Rotate(0, 0, -x);.
+		transform.position = transform.position + Vector3.right*x*6.0f*Time.deltaTime;
+
+		if (Mathf.Abs (x) > 0) {
+			ani.SetBool ("isMoving", true);	
+			/*if (rb.velocity.magnitude < speedLimit ) {
+				print (!wallCollision);
+				if (wallCollision) {
+					rb.velocity += Vector2.right * x * speedMultipler;	
+				} else {
+					rb.velocity += Vector2.right * x * speedMultipler/10;	
+				}
+			} */
+			if (x < 0)
+				GetComponent<SpriteRenderer> ().flipX = true;
+			else GetComponent<SpriteRenderer> ().flipX = false;
+		} else {
+			ani.SetBool ("isMoving", false);	
+		} 
 
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			CmdFire();
 		}
-		if (Input.GetKeyDown(KeyCode.Z))
+		if (Input.GetKeyDown(KeyCode.UpArrow))
 		{
-			PlayS();
+			if (wallCollision) {
+				Jump ();
+			} else {
+				
+			}
 		}
 	}
-	void PlayS() {
-	
+	void Jump() {
+		rb.velocity += Vector2.up * jumpPowerUp - collisionNormal*jumpPowerNormal;
 	}
+
 	[Command]
 	void CmdFire()
 	{
@@ -57,7 +91,38 @@ public class PlayerController : NetworkBehaviour{
 		// Destroy the bullet after 2 seconds
 		Destroy(bullet, 2.0f);        
 	}
+	void OnCollisionEnter2D(Collision2D col) {
+		if (isLocalPlayer) {
+			if (col.gameObject.tag == "Wall") {
+				wallCollision = true;
+				ani.SetBool ("onAir", false);
+				//GetComponent<Animator> ().SetBool ("onAir", false);
+				collisionNormal = (col.contacts [0].point - new Vector2 (transform.position.x, transform.position.y)).normalized;
+				print (collisionNormal);
 
+			}
+		}
+	}
+	void OnCollisionStay2D(Collision2D col) {
+		if (isLocalPlayer) {
+			if (col.gameObject.tag == "Wall") {
+				wallCollision = true;
+				//ani.SetBool ("onAir", false);	
+				GetComponent<Animator> ().SetBool ("onAir", false);
+				collisionNormal = (col.contacts [0].point - new Vector2 (transform.position.x, transform.position.y)).normalized;
+			}
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D col) {
+		if (isLocalPlayer) {
+			if (col.gameObject.tag == "Wall") {
+				ani.SetBool ("onAir", true);	
+				//GetComponent<Animator> ().SetBool ("onAir", true);
+				wallCollision = false;
+			}
+		}
+	}
 	public override void OnStartLocalPlayer()
 	{
 		GetComponent<SpriteRenderer>().color = Color.blue;
